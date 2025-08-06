c9507d7c47a451e51fa6093ef51bf3d46511834c
motiHaliva <motih9646@gmail.com>
2025-08-06 22:53:19 +0300

    Fix build errors and remove unused variables

--- express-app/migration.config.js
+++ express-app/migration.config.js
@@ -2,8 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  databaseUrl: process.env.DATABASE_URL,
-  ssl: true, 
+  databaseUrl: process.env.DATABASE_URL + '&ssl=true',
   migrationsTable: 'pgmigrations',
   dir: 'migrations',
 };
