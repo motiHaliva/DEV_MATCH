4738c1460cf7a9fbe926a311b417bb9083ec31ec
motiHaliva <motih9646@gmail.com>
2025-08-13 00:44:58 +0300

    Fix build errors and remove unused variables

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,16 +1,16 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
-  withCredentials: true,
-});
-
-export default axiosInstance;
 // import axios from "axios";
 
 // const axiosInstance = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
+//   baseURL: "http://localhost:4000",
 //   withCredentials: true,
 // });
 
 // export default axiosInstance;
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
+
+export default axiosInstance;
