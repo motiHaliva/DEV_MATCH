b5708eb1e84b566917aac3a59d405a25967cc95c
motiHaliva <motih9646@gmail.com>
2025-08-13 09:13:44 +0300

    Fix build errors and remove unused variables

--- client/src/ui/Louder.tsx
+++ client/src/ui/Louder.tsx
@@ -4,7 +4,7 @@ const Loader = () => (
   // <div className="flex flex-col items-center justify-center py-10">
   //   <div className="loader mb-4"></div>
   // </div>
-     <div className="p-8 bg-white rounded-2xl  border border-gray-100 text-center">
+     <div className="p-8 bg-white rounded-2xl  text-center">
                 <div className="animate-spin h-8 w-8 rounded-full border-b-2 border-blue-600 mx-auto mb-4"></div>
                 <p className="text-gray-500">Loading...</p>
             </div>
