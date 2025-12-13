47e76e16fb9c3d4cf34c5f9beb4101f0492f4799
motiHaliva <motih9646@gmail.com>
2025-12-13 23:52:33 +0200

    Update axios instance to conditionally set baseURL based on environment mode

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,20 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: import.meta.env.MODE === 'production'
+    ? "https://dev-match-oqi4.vercel.app"  
+    : "http://localhost:4000",              
   withCredentials: true,
 });
 
 export default axiosInstance;
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-//   withCredentials: true, 
-//   headers: {
-//     "Content-Type": "application/json",
-//     Accept: "application/json",
-//   },
-// });
-
-// export default axiosInstance;
