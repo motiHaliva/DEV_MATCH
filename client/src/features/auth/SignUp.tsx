81d7c66dfcf00837edfca512fa2b912d9c818cd4
motiHaliva <motih9646@gmail.com>
2025-12-15 09:53:50 +0200

    Refactor SignUp component: update form styling to include padding for better layout

--- client/src/features/auth/SignUp.tsx
+++ client/src/features/auth/SignUp.tsx
@@ -79,7 +79,7 @@ const SignUp = () => {
       ) : (
         <form
           onSubmit={handleSubmit}
-          className="text-center mt-9 shadow-xl md:w-1/2"
+          className="text-center mt-9 p-5 shadow-xl"
         >
           <img className="w-24 rounded-full mx-auto" src={logo} alt="logo" />
           <h1 className="text-2xl font-bold py-6">Create your account</h1>
